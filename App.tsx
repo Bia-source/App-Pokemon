77c126fa8f571af9512dd86007921d8b1f89d2e7
Bia-source <bia_ferreirads@yahoo.com>
2023-09-02 15:16:49 -0300

    definindo temas da aplicacao

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, NunitoSans_700Bold, NunitoSans_400Regular} from '@expo-google-fonts/nunito-sans'; 
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme/theme';
 
 export default function App() {
+  const [fontsLoad] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+         backgroundColor='transparent'
+         translucent
+      />
+      
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
